554b6a47a2f961c9d525a8cf987342f31dd78421
Carter Sifferman <cpsiff@gmail.com>
2018-05-06 20:22:32 -0500

    Added "bubbles" rule

--- rules.py
+++ rules.py
@@ -2,6 +2,7 @@
 # Carter Sifferman, 2018
 # For CSCI 474 Artificial Intelligence: Dr. Branton, Drury University
 
+import random
 
 # Cells die every round, are born if they have 3 neighbors.
 def seeds(grid, next_grid):
@@ -45,3 +46,26 @@ def life_without_death(grid, next_grid):
             if sum(neighbors) == 3:
                 grid[i][j] = 1
     return grid
+
+
+def bubbles(grid, next_grid):
+    for i in range(1, len(grid)-1):
+        for j in range(1, len(grid)-1):
+            if grid[i][j] == 1:
+                choice = random.choice([1, 2, 3, 4])
+
+                if choice == 1:                 # drift left
+                    next_grid[i-1][j+1] = 1
+
+                elif choice == 2:               # drift right
+                    next_grid[i+1][j+1] = 1
+
+                elif choice == 3:               # straight up
+                    next_grid[i][j+1] = 1
+
+                elif choice == 4:               # split or DIE
+                    if random.choice([0, 0, 0, 1, 1, 1, 1]) == 1:
+                        next_grid[i+1][j+1] = 1
+                        next_grid[i-1][j-1] = 1
+
+    return next_grid
